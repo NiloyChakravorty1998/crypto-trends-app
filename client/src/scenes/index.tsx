5daf9ec6771916cdaba223529e6e6ee0db44b8aa
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-11-14 23:38:36 +0530

    initial setup

---
+++ client/src/scenes/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const gridTemplateLargeScreens = `
+    "a b c"
+    "a b c"
+    "a b c"
+    "a b f"
+    "d e f"
+    "d e f"
+    "d h i"
+    "g h i"
+    "g h j"
+    "g h j"
+`;
+
+const gridTemplateSmallScreens = `
+"a"
+"a"
+"a"
+"a"
+"b"
+"b"
+"b"
+"b"
+"c"
+"c"
+"c"
+"d"
+"d"
+"d"
+"e"
+"e"
+"f"
+"f"
+"f"
+"g"
+"g"
+"g"
+"h"
+"h"
+"h"
+"h"
+"i"
+"i"
+"j"
+"j"
+`
+
+type Props = {}
+
+const index = (props: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
